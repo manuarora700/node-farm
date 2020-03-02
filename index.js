9d72e28107afa05c6ae02ac55830456d5592b581
Manu Arora <mannuarora700@gmail.com>
2020-03-02 15:01:46 +0530

    Http node server

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const fs = require("fs");
+const http = require("http");
 
-// Read a text file - synchronous (blocking)
+/////////////////////////////////////////////////////
+// Files
+
+Read a text file - synchronous (blocking)
 const output = fs.readFileSync("./txt/input.txt", "utf-8");
 console.log(output);
 
@@ -13,6 +17,7 @@ console.log("File has been written - blocking");
 // Non-blocking, Asynchronous way -- callbacks
 
 fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+  if (err) return console.log("error 😎");
   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
     console.log(data2);
     fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
@@ -23,3 +28,15 @@ fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
   });
 });
 // console.log("Will read file");
+
+///////////////////////////////////
+// SERVER
+
+const server = http.createServer((req, res) => {
+  console.log(req);
+  res.end("Helloooooooo from the server! 🤣");
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to requests on port 8000");
+});
